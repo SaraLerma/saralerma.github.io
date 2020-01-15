f2d3828c2411e394d8b8a8d2809609070c1ef084
SaraLerma <saralerma45@gmail.com>
2020-01-15 18:54:52 +0100

    fibonacci project

---
+++ fibonacci/precache-manifest.159f7207e67b537b2006540cd0f68a76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79757c7dfb2cfba5fdd036982a7668fc",
+    "url": "/fibonacci/index.html"
+  },
+  {
+    "revision": "1894949281fd5fd424e7",
+    "url": "/fibonacci/static/css/main.5539d330.chunk.css"
+  },
+  {
+    "revision": "14e68bafb957c5d71e2f",
+    "url": "/fibonacci/static/js/2.a8865814.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fibonacci/static/js/2.a8865814.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1894949281fd5fd424e7",
+    "url": "/fibonacci/static/js/main.7b1375e7.chunk.js"
+  },
+  {
+    "revision": "c5aeac4f7e79bd38a8ce",
+    "url": "/fibonacci/static/js/runtime-main.133268bd.js"
+  },
+  {
+    "revision": "996870930e8f8e628fe7e700702be080",
+    "url": "/fibonacci/static/media/coverwallet-logo.99687093.svg"
+  },
+  {
+    "revision": "d992b55914cced81f7667bcee2789a1e",
+    "url": "/fibonacci/static/media/formula.d992b559.png"
+  }
+]);
